0709b888f7e86a741cd3f184bc94b56041cd4be8
Sam Zanca <samuele@zanca.dev>
2023-07-23 10:13:47 -0400

    ratelimiting

--- src/lib/wfm/index.ts
+++ src/lib/wfm/index.ts
@@ -12,8 +12,19 @@
 import axios from 'axios';
 // @ts-ignore no type definitions for this package
 import axiosTauriAdapter from 'axios-tauri-adapter';
+import { TokenBucket } from '../rateLimiter';
 
 export const wfm = axios.create({
   adapter: axiosTauriAdapter,
   baseURL: 'https://api.warframe.market/v1',
 });
+
+// Look into how high burst can get before we get rate limited
+const rateLimiter = new TokenBucket(3, 2);
+
+export default {
+  async listItems() {
+    await rateLimiter.wait();
+    const { data } = await wfm.get('/items');
+  }
+}
